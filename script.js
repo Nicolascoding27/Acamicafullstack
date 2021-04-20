531d3b649dada4ad68bad42a0281e3c0efe23df8
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-04-20 18:53:25 -0500

    more js added

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-//casi no se usan
-let n=0
-do{
-    console.log('Do while', n)
-} while (n>=10)
-// For loop this loop repeats 10 times
-for(let i=0 ; i<10; i++){
-    console.log(i)
-}
-//operators 
-//n=n+1
-//n +=1 
-const color =['blue','Green','Black']
-for(let i=0 ;i<color.length;i++){
-    console.log(color[i])
-}
+// //casi no se usan
+// let n=0
+// do{
+//     console.log('Do while', n)
+// } while (n>=10)
+// // For loop this loop repeats 10 times
+// for(let i=0 ; i<10; i++){
+//     console.log(i)
+// }
+// //operators 
+// //n=n+1
+// //n +=1 
+const colors =['Blue','Green','Black']
 
+// for(let i=0 ;i<color.length;i++){
+//     console.log(color[i])
+// }
+///undefined si no conoce los valores que vienen 
+
+//No la conocia
+// for (const color of colors){
+//     console.log('color')
+// }
+
+//Switch 
+const producto ={
+    name:'Samsung Galaxy D20',
+    price:900,
+    freeshipping:false,
+    status:'ACTIVE'
+}
+//funcion basica para nombre 
+    function nombre(name){
+        console.log('Hello',name)
+    }
+    nombre('freddy')
+
+//fUNCION PARA CALCULAR DESCUENTO
+    // function promocion (price){
+    //     let percentage=_
+    // }
+// if(products.status==='Active'){
+//     console.log('El producto se encuentra activo')
+// } else if (products.status==='NO STOCK'){\
+//     console.log('El producto se encuentra SIN Stock')
+// ``}
+// else{
+//     console.log('El estado del producto no es correcto')
+// }
+
+// switch (product.status){
+//     case 'ACTIVE ';
+//     console.log('El producto se encuentra activo ')
+//     break;
+//     case 'Inactive';
+//     console.log('El producto se encuentra INACTIVO')
+// }
