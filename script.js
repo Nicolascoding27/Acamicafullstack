eb775ac335959b422a9f95af0354b116d6935455
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-04-20 18:20:20 -0500

    Js modifications

---
+++ script.js
@@ -0,0 +1,13 @@
+//casi no se usan
+let n=0
+do{
+    console.log('Do while', n)
+} while (n>=10)
+// For loop this loop repeats 10 times
+for(let i=0 ; i<10; i++){
+    console.log(i)
+}
+//operators 
+//n=n+1
+//n +=1 
+
