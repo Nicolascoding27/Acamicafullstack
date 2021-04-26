77912ccb25d4cbbe9a55a2b7d8c5e1c65a27d561
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-04-25 19:14:14 -0500

    infinite loop created  :(

---
+++ Clase 4 ejercicios JS/ejercicios.js
@@ -0,0 +1,34 @@
+//EJERCICIO NUMERO 1, CREAR UN PROMPT INDEFINIDO HASTA QUE EL USUARIO INGRESE STOP 
+// function arrayCheck(){
+//     const number=prompt('Ingresa un numero, me detendre si escribes STOP');
+//     // number ? ('STOP') : break;
+//     //lo que estoy intentando escribir es
+//     var array_numbers=[]
+//     var condition= number==='stop'? "true": "false"
+//     for(let i=0;condition==="false";i++ ){
+//     const number2=prompt('Ingresa un numero, me detendre si escribes STOP');
+//     array_numbers.push(number2)
+//     // number==="STOP" ? console.log (array_numbers) : arrayCheck() ;
+//     }
+//     return array_numbers
+// }
+//operador ternaario// ! con esto nego este statement //!!con esto es true 
+// function arrayCheck(){
+//     const number= prompt('type')
+//     const array=[];
+//  if(number="stop")return 
+//  else{
+//     number.push(array)
+//  }
+// }
+function arrayCheck(){
+   var number=prompt("inserta un numero")
+   var numbers=[]
+   while(number!=="stop"){
+      numbers.push(number)
+      var number2=prompt("inserta un numero")
+      numbers.push(number2)
+   }
+   return numbers
+}
+arrayCheck();
