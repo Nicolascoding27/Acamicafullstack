312ce4bf9b60cb9276ba5cb657cd31151783e66a
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-05-04 18:50:08 -0500

    some js added

---
+++ DOMmanipulation/script.js
@@ -0,0 +1,26 @@
+windows.onload=()=>{
+    const helements=document.getElementsByTagName()
+
+    const pquerry=document.querySelectorAll('description')
+    for(const p of pquery){
+        serInterval(()=>{
+            p.classList.remove('description')
+            p.className='primary description'//estoy es una clase
+            p.innerHtml=`
+            <div class="iner html">
+            <P>This is a test</p>`
+        },3000)
+    }
+    //class name vs class list 
+    // class list=array de clase 
+    // class name= just one class
+
+    const innerElemnts=document.querySelectorAll('.inner_elements')
+    //If I just want the first one
+    console.log('inner elements')
+    const
+}
+
+windows.onlo
+
+//Yo puedo usar el query selector tanto para clase como para Id's
