6ecf68325d8a95d051a5f8ce7d30ca8b4fb978de
Nicoing <nicolas.diaz@imagineapps.co>
2021-07-19 19:28:53 -0500

    More changes added

--- node/curseraCourse/index.js
+++ node/curseraCourse/index.js
@@ -5,19 +5,39 @@
 //     area:(x,y)=> (x*y)
 // }
 ///------Second way of doing it-------
-var rect=require('./rectangle')
 
+const rectangle = require("./rectangle")
+
+// var rect=require('./rectangle')
+
+// function solveRect(l,b){
+//     console.log('Solving for rect l='+ l + "and b=" + b)
+//     if(l<=0 || b<=0){
+//         console.log("Rectangle dimensions should be greater than zero")
+//     }
+//     else{
+//         console.log("The area of the rectangle is"+ " "+ rect.area(l,b))
+//         console.log("The area of the rectangle is" + " "+rect.perimeter(l,b))
+//     }
+// }
+// console.log(rect)
+// solveRect(2,4)
+// solveRect(0,5)
+// solveRect(-2,3)
+
+//----Callbacks and error handling---
+//--It's asynchronous so the values that come, come in an asynchronous way
 function solveRect(l,b){
     console.log('Solving for rect l='+ l + "and b=" + b)
-    if(l<=0 || b<=0){
-        console.log("Rectangle dimensions should be greater than zero")
-    }
-    else{
-        console.log("The area of the rectangle is"+ " "+ rect.area(l,b))
-        console.log("The area of the rectangle is" + " "+rect.perimeter(l,b))
-    }
+    rectangle(l,b,(err,rectangle)=> { //ya paso por parametro a l y b como x y Y no los tengo que volver a pasar mas adelante 
+        if(err){
+            console.log("Error",err.message)
+        }
+        else{
+            console.log('The area of the rectangle of' + l + "and" +b + "is" + rectangle.area());
+            console.log('The perimeter of the rectangle of' + l + "and" +b + "is" + rectangle.perimeter());
+        }
+    });
+    console.log('This comes after everything is completed')
 }
-console.log(rect)
-solveRect(2,4)
-solveRect(0,5)
-solveRect(-2,3)
+solveRect(0,2)
--- node/curseraCourse/rectangle.js
+++ node/curseraCourse/rectangle.js
@@ -1,4 +1,30 @@
-//We will create a node module 
-///Is better to have everything as modules 
-exports.perimeter=(x,y)=>(2*(x+y));
-exports.area=(x,y)=>(x*y)
+//We will create a node module
+///Is better to have everything as modules
+//--------------Exercise 1------------------
+
+// exports.perimeter=(x,y)=>(2*(x+y));
+// exports.area=(x,y)=>(x*y)
+
+//----------------Exercise 2-------
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    setTimeout(()=>
+    ///--The error message is attached to the error object => Like error.message
+    //Primer parametro error, segundo parametro funcion
+    callback(new Error("Rectangle dimensions should be greater than zero"),null)//Simulando error
+    ,2000);//simulation of a request, first parameter 
+    console.log();
+  }
+  else{
+    setTimeout(()=>
+    //Primer parametro error, segundo parametro funcion
+    callback(null,
+        {
+            //no tengo que pasar X y Y porque vienen de los atributos
+            perimeter:()=>(2*(x+y)), 
+            area:()=>(x*y) // no tengo que pasar x y y en el area porque vienen como parametros
+        }
+        ),2000
+    )
+  }
+};
