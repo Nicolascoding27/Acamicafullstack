0d1f34cd11a9d941fea897756677203aa9ffb52f
Nicoing <nicolas.diaz@imagineapps.co>
2021-07-21 09:22:21 -0500

    Express excersice added

---
+++ node/curseraExpress/index.js
@@ -0,0 +1,17 @@
+const express=require('express'); //Automatically included from the node modules 
+const http=require('http');
+
+const hostname='localhost';
+const port= 3000;
+const app= express();
+
+app.use((req,res,next)=>{
+    console.log('>>>headers'+ JSON.stringify(req.headers))
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html')
+    res.end('<html><body><h1>This is an Express Server </h1></body></html>')
+});
+const server =http.createServer(app);
+server.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}`)
+})
